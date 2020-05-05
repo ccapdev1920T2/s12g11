186c94ca4a73f14f3c93097c75f2a583aed688b9
Ygg <dondon_tan@dlsu.edu.ph>
2020-05-05 21:50:39 +0800

    prevent key input in edit

--- public/js/profile.js
+++ public/js/profile.js
@@ -81,6 +81,13 @@ $(document).ready(function () {
 				}
                 
             });
+			
+			$('input').on('keydown', function(event) {
+				var x = event.which;
+				if (x === 13) {
+					event.preventDefault();
+				}
+			});
         }
     });
 
